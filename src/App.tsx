5aace45f191329588baf0d81329a94876004cdfb
Sung <sung272012@hotmail.com>
2024-10-05 16:07:19 -0400

    Set up background and logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react';
+import video from './assets/videos/background-video.mp4';
+import logo from './assets/img/ghibli-logo.png';
+import './styles/index.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className='relative flex items-center justify-center h-screen overflow-hidden'>
+      <video
+        className='absolute -z-50 w-auto min-w-full min-h-full max-w-none'
+        autoPlay
+        loop
+        muted
+      >
+        <source src={video} type='video/mp4'></source>
+      </video>
+      <img className='absolute w-80 top-8 left-8' src={logo} alt='logo image' />
+    </div>
+  );
 }
 
-export default App
+export default App;
